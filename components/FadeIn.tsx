974756e2d5fa2eb63bf973ced4eb18e5ade1f0ac
Yut8853 <hello@junkbranding.com>
2025-01-09 19:36:10 +0900

    型修正

--- components/FadeIn.tsx
+++ components/FadeIn.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext } from 'react';
 import { motion, MotionProps } from 'framer-motion'; // 修正：framer-motion を使用
 
 // コンテキストの作成
@@ -7,50 +7,45 @@ const fadeInStaggerContext = createContext(false);
 // ビューポート設定
 const viewport = { once: true, margin: '0px 0px -200px' };
 
-// FadeIn コンポーネントの型定義
-type FadeInProps = MotionProps; // motion.div に適用可能なすべてのプロパティを許容
+type FadeInProps = MotionProps & {
+  className?: string;
+};
 
-function FadeIn(props: FadeInProps) {
-  const isInStaggerGroup = useContext(fadeInStaggerContext); // コンテキストの値を取得
-
+const FadeIn: React.FC<FadeInProps> = ({ className, ...props }) => {
   return (
     <motion.div
+      className={className}
       variants={{
-        hidden: { opacity: 0, y: 20 }, // 初期状態
-        visible: { opacity: 1, y: 0 }, // アニメーション終了状態
+        hidden: { opacity: 0, y: 20 },
+        visible: { opacity: 1, y: 0 },
       }}
-      transition={{ duration: 0.5 }} // アニメーションの継続時間
-      {...(isInStaggerGroup
-        ? {}
-        : {
-            initial: 'hidden', // 初期状態
-            whileInView: 'visible', // ビューポート内で表示
-            viewport, // ビューポート設定
-          })}
-      {...props} // その他のプロパティを渡す
+      transition={{ duration: 0.5 }}
+      {...props}
     />
   );
-}
+};
 
 export default FadeIn;
 
-// FadeInStagger コンポーネントの型定義
 type FadeInStaggerProps = MotionProps & {
-  faster?: boolean; // 追加オプション
+  faster?: boolean;
+  className?: string;
 };
 
 export const FadeInStagger: React.FC<FadeInStaggerProps> = ({
   faster = false,
+  className,
   ...props
 }) => {
   return (
     <fadeInStaggerContext.Provider value={true}>
       <motion.div
-        initial="hidden" // 初期状態
-        whileInView="visible" // ビューポート内で表示
-        viewport={viewport} // ビューポート設定
-        transition={{ staggerChildren: faster ? 0.12 : 0.2 }} // スタッガーアニメーション
-        {...props} // その他のプロパティを渡す
+        className={className}
+        initial="hidden"
+        whileInView="visible"
+        viewport={viewport}
+        transition={{ staggerChildren: faster ? 0.12 : 0.2 }}
+        {...props}
       />
     </fadeInStaggerContext.Provider>
   );
