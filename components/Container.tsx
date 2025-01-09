8fa047837fb0cce086a7b10c6121d11f540fbcb8
Yut8853 <hello@junkbranding.com>
2025-01-09 19:20:58 +0900

    型修正

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { ReactNode } from 'react';
 
-function Container({children}) {
-  return (
-    <div className='max-w-screen-2xl mx-auto px-2 py-10'>{children}</div>
-  )
+type ContainerProps = {
+  children: ReactNode;
+};
+
+function Container({ children }: ContainerProps) {
+  return <div className="max-w-screen-2xl mx-auto px-2 py-10">{children}</div>;
 }
 
-export default Container
+export default Container;
